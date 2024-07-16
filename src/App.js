0d61e0b9b10a401fe217619fa1a9474efa8ef266
klusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2024-07-16 17:55:34 -0300

    criaçaõ do primeiro component 'card' com estilziação Emotion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { Card } from './components/Card/Card';
 import { Estilos } from './components/EstilosGlobais/Estilos';
 
 function App() {
   return (
     <div>
       <Estilos />
-      <h1>Freelando</h1>
+      <Card>
+        <h1>Freelando</h1>
+      </Card>
     </div>
   );
 }
